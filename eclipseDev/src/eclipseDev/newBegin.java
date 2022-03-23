9e0f72e82e473e972e2d9fe2fa2419b517a78838
ycan <yasemin.can@saglik.gov.tr>
2022-03-23 12:21:32 +0300

    secondary commit tiral

---
+++ eclipseDev/src/eclipseDev/newBegin.java
@@ -0,0 +1,15 @@
+package eclipseDev;
+
+public class newBegin {
+
+	public newBegin() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("today is a nice day to begin a new advanture");
+
+	}
+
+}
